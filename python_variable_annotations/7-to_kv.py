bf260e37a4ba543e23b970aacc2a08d1fe6867fd
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-10 21:56:49 +0300

    a

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for creating a tuple from a 
+"""Module for creating a tuple from a
 string and the square of a number."""
 
 from typing import Union, Tuple
@@ -7,7 +7,7 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple with a string and 
+    Returns a tuple with a string and
     the square of an int or float as a float.
 
     Args:
