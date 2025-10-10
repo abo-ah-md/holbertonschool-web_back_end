4c5d05c6a68426ad794a4ac80034915fbcf2e0de
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-10 21:40:40 +0300

    a

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this is a module that concat 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """this is afunction that returns 2 strings concatenated"""
+    return str1 + str2
