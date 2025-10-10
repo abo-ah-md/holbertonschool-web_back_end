e1935e3dc3441eb75206d606f0655966ccb29e0c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-10 22:13:50 +0300

    a

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,8 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Coroutine that asynchronously yields
-    10 random numbers between 0 and 10, waiting 1s between each.
+    10 random numbers between 0 and 10,
+    waiting 1s between each.
 
     Yields:
         float: A random float between 0 and 10.
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module to measure runtime for parallel async comprehensions."""
+"""Module to measure runtime for
+parallel async comprehensions."""
 
 import asyncio
 import time
@@ -10,10 +11,12 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """
-    Executes async_comprehension 4 times in parallel and measures the total runtime.
+    Executes async_comprehension 4
+    times in parallel and measures the total runtime.
 
     Returns:
-        float: The total time taken to run 4 async_comprehension coroutines in parallel.
+        float: The total time taken to run
+        4 async_comprehension coroutines in parallel.
     """
     start = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
