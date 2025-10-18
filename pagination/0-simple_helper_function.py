356f11740942810015592c2313b7a91bf0b7227a
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-19 01:18:18 +0300

    added a helper function for page indexing

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/bin/user/env/python3
+"""this is a helper function for page indexing"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """on every next page the index will incrament to the size"""
+    start_index: int
+    end_index: int
+    if page == 1:
+        start_index = 0
+    else:
+        start_index = (page_size * page) - page_size
+    end_index = page_size * page
+    return (start_index, end_index)
