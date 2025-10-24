ab3306c06809d373861b319d0e214b4b17c259e7
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-24 22:49:21 +0300

    addded a scriprt to  create or use  no sql monog db's

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB"""
+"""Display stats about nginx"""
 from pymongo import MongoClient
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+def display_stats(nginx_collection):
+    """Display stats about nginx"""
+    print(f"{nginx_collection.estimated_document_count()} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+    status_check_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check_count} status check")
+
 
 if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection = client.logs.nginx
-
-    total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method in METHODS:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
+    display_stats(client.logs.nginx)
