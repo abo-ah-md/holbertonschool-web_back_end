4c23bb4dde46aeb2fe54c2156a5a047146c8936b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-24 22:52:03 +0300

    addded a scriprt to  create or use  no sql monog db's

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
-"""Display stats about nginx"""
+""" this module provides some stats about nginx logs """
 from pymongo import MongoClient
 
 
-def display_stats(nginx_collection):
-    """Display stats about nginx"""
-    print(f"{nginx_collection.estimated_document_count()} logs")
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        method_count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_count}")
-    status_check_count = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
+def main():
+    """provides some stats about nginx logs"""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    list_data = list(collection.find())
+
+    m_get, m_post, m_put = 0, 0, 0
+    m_patch, m_delete, s_check = 0, 0, 0
+
+    for i in list_data:
+        if i.get("method") is None:
+            continue
+        elif i.get("method") == "GET":
+            m_get += 1
+            if i.get("path") == "/status":
+                s_check += 1
+        elif i.get("method") == "POST":
+            m_post += 1
+        elif i.get("method") == "PUT":
+            m_put += 1
+        elif i.get("method") == "PATCH":
+            m_patch += 1
+        elif i.get("method") == "DELETE":
+            m_delete += 1
+
+    logs = len(list_data)
+    print(f"{logs} logs")
+    print(
+    f"Methods:\n\tmethod GET: {m_get}"+
+    f"\n\tmethod POST: {m_post}\n\tmethod PUT: {m_put}"+
+    f"\n\tmethod PATCH: {m_patch}\n\tmethod DELETE: {m_delete}"
     )
-    print(f"{status_check_count} status check")
+    print(f"{s_check} status check")
 
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    display_stats(client.logs.nginx)
+    main()
