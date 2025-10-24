300b341cdc8bd7ec09899dc6c478b2a22743e3db
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-24 22:55:01 +0300

    addded a scriprt to  create or use  no sql monog db's

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,43 +1,20 @@
 #!/usr/bin/env python3
-"""this module provides some stats about nginx logs"""
+"""
+Script that provides some stats about Nginx logs stored in MongoDB.
+"""
 from pymongo import MongoClient
 
 
-def main():
-    """provides some stats about nginx logs"""
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    collection = client.logs.nginx
-
-    list_data = list(collection.find())
-
-    m_get, m_post, m_put = 0, 0, 0
-    m_patch, m_delete, s_check = 0, 0, 0
-
-    for i in list_data:
-        if i.get("method") is None:
-            continue
-        elif i.get("method") == "GET":
-            m_get += 1
-            if i.get("path") == "/status":
-                s_check += 1
-        elif i.get("method") == "POST":
-            m_post += 1
-        elif i.get("method") == "PUT":
-            m_put += 1
-        elif i.get("method") == "PATCH":
-            m_patch += 1
-        elif i.get("method") == "DELETE":
-            m_delete += 1
-
-    logs = len(list_data)
-    print(f"{logs} logs")
-    print(
-        f"Methods:\n\tmethod GET: {m_get}"
-        + f"\n\tmethod POST: {m_post}\n\tmethod PUT: {m_put}"
-        + f"\n\tmethod PATCH: {m_patch}\n\tmethod DELETE: {m_delete}"
-    )
-    print(f"{s_check} status check")
-
-
 if __name__ == "__main__":
-    main()
+    client = MongoClient('mongodb://localhost:27017')
+    collec = client.logs.nginx
+
+    print(f'{collec.count_documents({})} logs')
+    print('Methods:')
+
+    print(f'\tmethod GET: {collec.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {collec.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {collec.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {collec.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {collec.count_documents({"method": "DELETE"})}')
+    print(f'{collec.count_documents({"path": "/status"})} status check')
