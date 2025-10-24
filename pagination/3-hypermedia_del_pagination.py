59f9689716edd1a49ce99df6bb2e6a18110a372c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-24 21:22:06 +0300

    added a hypermedia method to server class

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -32,7 +32,7 @@ class Server:
             self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
         """
         Return a dictionary containing pagination info resilient to deletions.
         Ensures that:
