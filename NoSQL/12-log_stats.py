b647d25f2c3909c7eec2c595f03921a6fbaf82c4
abo-ah-md <abo-ah-md1@hotmail.com>
2025-10-24 22:44:02 +0300

    addded a scriprt to  create or use  no sql monog db's

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,30 +2,20 @@
 """Provides stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats(mongo_collection, option=None):
-    """Displays stats about Nginx logs"""
-    if option:
-        value = mongo_collection.count_documents({"method": option})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    total_logs = mongo_collection.count_documents({})
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(mongo_collection, method)
-
-    status_check = mongo_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
-
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    log_stats(nginx_collection)
+
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in METHODS:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check} status check")
